763aee332db6afdfc24cf1268667fe68c9975704
Anwar1477 <joy.cse017@gmail.com>
2022-04-17 14:58:31 +0600

    header and home page done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-            <nav className='color: red'>
-                <Link to='/'>Home</Link>
-                <Link to='/services'>Services</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/blogs'>Blogs</Link>
-                <Link to='/login'>Login</Link>
-
-            </nav>
-    );
+  return (
+    <div className="header-area">
+      <nav className="d-flex justify-content-around">
+        <div className="title">
+          <h1>
+            Power <span>Zone</span>
+          </h1>
+        </div>
+        <Link className="link" to="/home">Home</Link>
+        <Link className="link" to="/services">Services</Link>
+        <Link className="link" to="/about">About</Link>
+        <Link className="link" to="/blogs">Blogs</Link>
+        <Link className="link" to="/login">Login</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
